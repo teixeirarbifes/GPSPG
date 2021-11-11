704dcf2bf472cb4883757355730a1df956e322a7
Rafael <teixeira.rb.ifes@gmail.com>
2021-11-11 11:05:21 -0300

    no message

--- conf.php
+++ conf.php
@@ -6,20 +6,15 @@ else
 define('S','\\');
 define('GPATH',$gpath.S);
                    
-//define('STRING_DB',"mysql:host=mocha3036.mochahost.com;port=3306;dbname=gemadne2_gpspg");        
-//define('USUARIO_DB',"gemadne2_gpspg");        
-//define('SENHA_DB',"Rafa1064*");    
+define('STRING_DB',"mysql:host=server;port=3306;dbname=dbname");        
+define('USUARIO_DB',"usuario");        
+define('SENHA_DB',"senha");    
 
-
-define('STRING_DB',"mysql:host=mocha3036.mochahost.com;port=3306;dbname=gemadne2_gpspgdev");        
-define('USUARIO_DB',"gemadne2_gpspgdev");        
-define('SENHA_DB',"afszJZHHIsh,");    
-
-define('SMTP_SERVER','mocha3036.mochahost.com');
-define('SMTP_USER','gpspg@gemad.net');
-define('SMTP_PASS','S@V~T80!bt71');
-define('SMTP_MAIL','gpspg@gemad.net');
-define('SMTP_CONFIRM','gpspg@gemad.net');
+define('SMTP_SERVER','server');
+define('SMTP_USER','user');
+define('SMTP_PASS','senha');
+define('SMTP_MAIL','email');
+define('SMTP_CONFIRM','email2');
 
 define('DESENVOLVIMENTO',1);
 fclose($myfile);
