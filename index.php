f3e3e2a49276ac09e62b91c897d1f6d1cf493aa8
Rafael <teixeira.rb.ifes@gmail.com>
2021-11-11 13:06:29 -0300

    init2

--- index.php
+++ index.php
@@ -168,6 +168,7 @@ ob_start();
 				}
 			}
 		</script>
+    teste2
   <div class="container-scroller">
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
